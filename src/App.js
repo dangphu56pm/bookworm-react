96a2c599ab23bcebb80e687e690b7da30957e61d
phund7 <phund7@fpt.com.vn>
2017-10-26 21:27:20 +0700

    Singup Form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
 import DashboardPage from './components/pages/DashboardPage';
+import SignupPage from './components/pages/SignupPage';
 import UserRoute from './components/routes/UserRoute';
 import GuestRoute from './components/routes/GuestRoute';
 
@@ -11,6 +12,7 @@ const App = ({ location }) => (
   <div className="ui container">
     <Route location={location} path="/" exact component={HomePage} />
     <GuestRoute location={location} path="/login" component={LoginPage} />
+    <GuestRoute location={location} path="/signup" component={SignupPage} />
     <UserRoute location={location} path="/dashboard" component={DashboardPage} />
   </div>
 );
